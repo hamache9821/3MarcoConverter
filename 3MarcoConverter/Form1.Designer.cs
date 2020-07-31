e3c2c134314c0febb77f4708a0c7bdd0141e7355
hamache9801 <hamache9821@gmail.com>
2020-08-01 05:46:56 +0900

    initial

---
+++ 3MarcoConverter/Form1.Designer.cs
@@ -0,0 +1,458 @@
+﻿namespace _3MarcoConverter
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.num_3marcoS = new System.Windows.Forms.NumericUpDown();
+			this.num_3marcoM = new System.Windows.Forms.NumericUpDown();
+			this.num_3marcoD = new System.Windows.Forms.NumericUpDown();
+			this.num_3marcoH = new System.Windows.Forms.NumericUpDown();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.num_TargetS = new System.Windows.Forms.NumericUpDown();
+			this.num_TargetM = new System.Windows.Forms.NumericUpDown();
+			this.num_TargetD = new System.Windows.Forms.NumericUpDown();
+			this.num_TargetH = new System.Windows.Forms.NumericUpDown();
+			this.button1 = new System.Windows.Forms.Button();
+			this.lbl_Result = new System.Windows.Forms.Label();
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			this.label9 = new System.Windows.Forms.Label();
+			this.button2 = new System.Windows.Forms.Button();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.label10 = new System.Windows.Forms.Label();
+			this.label11 = new System.Windows.Forms.Label();
+			this.groupBox3 = new System.Windows.Forms.GroupBox();
+			this.groupBox1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.num_3marcoS)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.num_3marcoM)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.num_3marcoD)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.num_3marcoH)).BeginInit();
+			this.groupBox2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.num_TargetS)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.num_TargetM)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.num_TargetD)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.num_TargetH)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			this.groupBox3.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.label3);
+			this.groupBox1.Controls.Add(this.label2);
+			this.groupBox1.Controls.Add(this.label7);
+			this.groupBox1.Controls.Add(this.label1);
+			this.groupBox1.Controls.Add(this.num_3marcoS);
+			this.groupBox1.Controls.Add(this.num_3marcoM);
+			this.groupBox1.Controls.Add(this.num_3marcoD);
+			this.groupBox1.Controls.Add(this.num_3marcoH);
+			this.groupBox1.Location = new System.Drawing.Point(18, 16);
+			this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+			this.groupBox1.Size = new System.Drawing.Size(369, 65);
+			this.groupBox1.TabIndex = 1;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "3Marco = ";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(346, 28);
+			this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(15, 16);
+			this.label3.TabIndex = 1;
+			this.label3.Text = "s";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(256, 28);
+			this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(20, 16);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "m";
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Location = new System.Drawing.Point(79, 28);
+			this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(16, 16);
+			this.label7.TabIndex = 1;
+			this.label7.Text = "d";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(166, 28);
+			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(16, 16);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "h";
+			// 
+			// num_3marcoS
+			// 
+			this.num_3marcoS.Location = new System.Drawing.Point(280, 24);
+			this.num_3marcoS.Margin = new System.Windows.Forms.Padding(4);
+			this.num_3marcoS.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+			this.num_3marcoS.Name = "num_3marcoS";
+			this.num_3marcoS.Size = new System.Drawing.Size(62, 23);
+			this.num_3marcoS.TabIndex = 3;
+			this.num_3marcoS.TabStop = false;
+			// 
+			// num_3marcoM
+			// 
+			this.num_3marcoM.Location = new System.Drawing.Point(190, 24);
+			this.num_3marcoM.Margin = new System.Windows.Forms.Padding(4);
+			this.num_3marcoM.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+			this.num_3marcoM.Name = "num_3marcoM";
+			this.num_3marcoM.Size = new System.Drawing.Size(62, 23);
+			this.num_3marcoM.TabIndex = 2;
+			this.num_3marcoM.TabStop = false;
+			// 
+			// num_3marcoD
+			// 
+			this.num_3marcoD.Location = new System.Drawing.Point(13, 24);
+			this.num_3marcoD.Margin = new System.Windows.Forms.Padding(4);
+			this.num_3marcoD.Name = "num_3marcoD";
+			this.num_3marcoD.Size = new System.Drawing.Size(62, 23);
+			this.num_3marcoD.TabIndex = 0;
+			this.num_3marcoD.TabStop = false;
+			// 
+			// num_3marcoH
+			// 
+			this.num_3marcoH.Location = new System.Drawing.Point(100, 24);
+			this.num_3marcoH.Margin = new System.Windows.Forms.Padding(4);
+			this.num_3marcoH.Maximum = new decimal(new int[] {
+            23,
+            0,
+            0,
+            0});
+			this.num_3marcoH.Name = "num_3marcoH";
+			this.num_3marcoH.Size = new System.Drawing.Size(62, 23);
+			this.num_3marcoH.TabIndex = 1;
+			this.num_3marcoH.TabStop = false;
+			this.num_3marcoH.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Controls.Add(this.label4);
+			this.groupBox2.Controls.Add(this.label5);
+			this.groupBox2.Controls.Add(this.label8);
+			this.groupBox2.Controls.Add(this.label6);
+			this.groupBox2.Controls.Add(this.num_TargetS);
+			this.groupBox2.Controls.Add(this.num_TargetM);
+			this.groupBox2.Controls.Add(this.num_TargetD);
+			this.groupBox2.Controls.Add(this.num_TargetH);
+			this.groupBox2.Location = new System.Drawing.Point(18, 101);
+			this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
+			this.groupBox2.Size = new System.Drawing.Size(369, 65);
+			this.groupBox2.TabIndex = 0;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "Target =";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(346, 28);
+			this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(15, 16);
+			this.label4.TabIndex = 1;
+			this.label4.Text = "s";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(256, 28);
+			this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(20, 16);
+			this.label5.TabIndex = 1;
+			this.label5.Text = "m";
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Location = new System.Drawing.Point(80, 28);
+			this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(16, 16);
+			this.label8.TabIndex = 1;
+			this.label8.Text = "d";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(166, 28);
+			this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(16, 16);
+			this.label6.TabIndex = 1;
+			this.label6.Text = "h";
+			// 
+			// num_TargetS
+			// 
+			this.num_TargetS.Location = new System.Drawing.Point(280, 24);
+			this.num_TargetS.Margin = new System.Windows.Forms.Padding(4);
+			this.num_TargetS.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+			this.num_TargetS.Name = "num_TargetS";
+			this.num_TargetS.Size = new System.Drawing.Size(62, 23);
+			this.num_TargetS.TabIndex = 3;
+			// 
+			// num_TargetM
+			// 
+			this.num_TargetM.Location = new System.Drawing.Point(190, 24);
+			this.num_TargetM.Margin = new System.Windows.Forms.Padding(4);
+			this.num_TargetM.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+			this.num_TargetM.Name = "num_TargetM";
+			this.num_TargetM.Size = new System.Drawing.Size(62, 23);
+			this.num_TargetM.TabIndex = 2;
+			// 
+			// num_TargetD
+			// 
+			this.num_TargetD.Location = new System.Drawing.Point(14, 24);
+			this.num_TargetD.Margin = new System.Windows.Forms.Padding(4);
+			this.num_TargetD.Name = "num_TargetD";
+			this.num_TargetD.Size = new System.Drawing.Size(62, 23);
+			this.num_TargetD.TabIndex = 0;
+			// 
+			// num_TargetH
+			// 
+			this.num_TargetH.Location = new System.Drawing.Point(100, 24);
+			this.num_TargetH.Margin = new System.Windows.Forms.Padding(4);
+			this.num_TargetH.Maximum = new decimal(new int[] {
+            23,
+            0,
+            0,
+            0});
+			this.num_TargetH.Name = "num_TargetH";
+			this.num_TargetH.Size = new System.Drawing.Size(62, 23);
+			this.num_TargetH.TabIndex = 1;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(18, 204);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(95, 36);
+			this.button1.TabIndex = 3;
+			this.button1.Text = "変換！";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// lbl_Result
+			// 
+			this.lbl_Result.AutoSize = true;
+			this.lbl_Result.Font = new System.Drawing.Font("MS UI Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.lbl_Result.Location = new System.Drawing.Point(121, 192);
+			this.lbl_Result.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.lbl_Result.Name = "lbl_Result";
+			this.lbl_Result.Size = new System.Drawing.Size(0, 29);
+			this.lbl_Result.TabIndex = 1;
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.Location = new System.Drawing.Point(76, 174);
+			this.numericUpDown1.Margin = new System.Windows.Forms.Padding(4);
+			this.numericUpDown1.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(37, 23);
+			this.numericUpDown1.TabIndex = 2;
+			this.numericUpDown1.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.Location = new System.Drawing.Point(16, 177);
+			this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(56, 16);
+			this.label9.TabIndex = 1;
+			this.label9.Text = "有効桁";
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(14, 22);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(96, 52);
+			this.button2.TabIndex = 3;
+			this.button2.Text = "開始";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// label10
+			// 
+			this.label10.AutoSize = true;
+			this.label10.Font = new System.Drawing.Font("MS UI Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.label10.Location = new System.Drawing.Point(117, 66);
+			this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(89, 29);
+			this.label10.TabIndex = 1;
+			this.label10.Text = "Marco";
+			// 
+			// label11
+			// 
+			this.label11.AutoSize = true;
+			this.label11.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.label11.Location = new System.Drawing.Point(120, 22);
+			this.label11.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label11.Name = "label11";
+			this.label11.Size = new System.Drawing.Size(86, 27);
+			this.label11.TabIndex = 4;
+			this.label11.Text = "Sec = ";
+			// 
+			// groupBox3
+			// 
+			this.groupBox3.Controls.Add(this.button2);
+			this.groupBox3.Controls.Add(this.label11);
+			this.groupBox3.Controls.Add(this.label10);
+			this.groupBox3.Location = new System.Drawing.Point(0, 259);
+			this.groupBox3.Name = "groupBox3";
+			this.groupBox3.Size = new System.Drawing.Size(415, 115);
+			this.groupBox3.TabIndex = 5;
+			this.groupBox3.TabStop = false;
+			this.groupBox3.Text = "リアルタイム変換";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(427, 383);
+			this.Controls.Add(this.groupBox3);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.groupBox2);
+			this.Controls.Add(this.label9);
+			this.Controls.Add(this.lbl_Result);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.numericUpDown1);
+			this.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.Margin = new System.Windows.Forms.Padding(4);
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "Form1";
+			this.ShowIcon = false;
+			this.Text = "3MarcoConverter";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.num_3marcoS)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.num_3marcoM)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.num_3marcoD)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.num_3marcoH)).EndInit();
+			this.groupBox2.ResumeLayout(false);
+			this.groupBox2.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.num_TargetS)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.num_TargetM)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.num_TargetD)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.num_TargetH)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			this.groupBox3.ResumeLayout(false);
+			this.groupBox3.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.NumericUpDown num_3marcoS;
+		private System.Windows.Forms.NumericUpDown num_3marcoM;
+		private System.Windows.Forms.NumericUpDown num_3marcoH;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.NumericUpDown num_TargetS;
+		private System.Windows.Forms.NumericUpDown num_TargetM;
+		private System.Windows.Forms.NumericUpDown num_TargetH;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label lbl_Result;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.NumericUpDown num_3marcoD;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.NumericUpDown num_TargetD;
+		private System.Windows.Forms.NumericUpDown numericUpDown1;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.Label label11;
+		private System.Windows.Forms.GroupBox groupBox3;
+	}
+}
+
---
+++ 3MarcoConverter/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3MarcoConverter
+{
+	public partial class Form1 : Form
+	{
+		private decimal count = 0;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			timer1.Interval = 1000;
+			timer1.Tick += Timer1_Tick;
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				var tmp = 3 / toSecond(num_3marcoD.Value, num_3marcoH.Value, num_3marcoM.Value, num_3marcoS.Value);
+				var resut = toSecond(num_TargetD.Value, num_TargetH.Value, num_TargetM.Value, num_TargetS.Value) * tmp;
+
+				lbl_Result.Text = format(resut);
+			}
+			catch
+			{
+				lbl_Result.Text = "3Marcoが未定義";
+			}
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			timer1.Enabled = !timer1.Enabled;
+
+			if (timer1.Enabled)
+			{
+				count = toSecond(num_TargetD.Value, num_TargetH.Value, num_TargetM.Value, num_TargetS.Value);
+				timer1.Start();
+				button2.Text = "停止";
+			}
+			else
+			{
+				timer1.Stop();
+				button2.Text = "開始";
+			}
+		}
+
+		private void Timer1_Tick(object sender, EventArgs e)
+		{
+			try
+			{
+				var tmp = 3 / toSecond(num_3marcoD.Value, num_3marcoH.Value, num_3marcoM.Value, num_3marcoS.Value);
+				var resut = (count++) * tmp;
+
+				setValue(label11, $"{toTimeSpan(count)}=");
+				setValue(label10, format(resut));
+			}
+			catch
+			{
+			}
+		}
+
+		private void setValue(Label c, string o)
+		{
+			if (c.InvokeRequired)
+			{
+				setValue(c, o);
+			}else
+			{
+				c.Text = o;
+			}
+		}
+
+		private decimal toSecond(decimal d, decimal h, decimal m, decimal s)
+		{
+			return (d * 86400) + (h * 3600) + (m * 60) + s;
+		}
+
+		private string toTimeSpan(decimal i)
+		{
+			return new TimeSpan(0, 0, (int)i).ToString();
+		}
+
+		private string format(decimal i)
+		{
+			return (i.ToString("N10") == "1.5000000000") ? "半marco" : i.ToString($"N{numericUpDown1.Value}") + "Marco";
+		}
+	}
+}
